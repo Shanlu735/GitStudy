4af5dcf979df8f153da1922cc04a9518ad3df0d9
齐小鲁 <1556225479@qq.com>
2021-01-21 11:09:33 +0800

    完成了登录

---
+++ vueD/vue-02/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+//导入全局样式表
+import './assets/css/global.css'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
